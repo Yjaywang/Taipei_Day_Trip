1ee2a967091a0881756d2f78a207ba036d9acf6a
Jay Wang <E34013160@gmail.com>
2022-11-25 23:14:25 +0800

    test done, replace original app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def thankyou():
 def all_exception_handler(error):
     res = {
 		"error": True,
-		"message": "伺服器內部錯誤"
+		"message": "error message: server error"  #+str(error)
 		}
     return Response(status=500, mimetype="application/json", response=json.dumps(res))
 
@@ -41,14 +41,24 @@ def attractions():
 	display=12
 	try:       	
 		connection = connection_pool.get_connection()
+		
 		if keyword:  #if have keyword
-			mySql_query_count = ("""SELECT count(*) FROM site
-								WHERE CAT=%s or name LIKE %s """
-			)
-			mySql_query = ("""SELECT * FROM site
-							WHERE CAT=%s or name LIKE %s 
-							LIMIT %s, %s """
-			)			
+			mySql_query_count = ("""
+				SELECT count(*)
+				FROM attraction as a 
+				INNER JOIN category as c on c.id = a.CAT_id
+				WHERE c.CAT=%s or a.name LIKE %s
+				""")
+			mySql_query = ("""
+				SELECT s._id, s.name, s.CAT, s.description, s.address, s.direction, m.MRT, s.latitude, s.longitude, i.file
+				FROM (SELECT a._id, a.id, a.MRT_id, a.name, c.CAT, a.description, a.address, a.direction, a.latitude, a.longitude 
+					FROM attraction as a
+					INNER JOIN category as c on c.id = a.CAT_id
+					WHERE c.CAT= %s or a.name LIKE %s
+					LIMIT %s, %s) as s
+				INNER JOIN mrt as m on m.id=s.MRT_id
+				INNER JOIN image as i on i.attr_id = s.id
+				""")			
 			if connection.is_connected():
 				count_cursor = connection.cursor()
 				count_cursor.execute(mySql_query_count, (keyword,"%"+keyword+"%"))   
@@ -62,10 +72,19 @@ def attractions():
 				records=cursor.fetchall()   
 				cursor.close()   
 				output=[] #use for return 
-			
+				id_check_list=[]
+		
 		else:
-			mySql_query_count = ("""SELECT count(*) FROM site""")
-			mySql_query = ("""SELECT * FROM site LIMIT %s, %s """)			
+			mySql_query_count = ("""SELECT count(*) FROM attraction""")
+			mySql_query = ("""
+				SELECT s._id, s.name, s.CAT, s.description, s.address, s.direction, m.MRT, s.latitude, s.longitude, i.file
+				FROM (SELECT a._id, a.id, a.MRT_id, a.name, c.CAT, a.description, a.address, a.direction, a.latitude, a.longitude 
+					FROM attraction as a
+					INNER JOIN category as c on c.id = a.CAT_id
+					LIMIT %s, %s) as s
+				INNER JOIN mrt as m on m.id=s.MRT_id
+				INNER JOIN image as i on i.attr_id = s.id
+				""")			
 			if connection.is_connected():
 				count_cursor = connection.cursor()
 				count_cursor.execute(mySql_query_count)   
@@ -79,21 +98,42 @@ def attractions():
 				records=cursor.fetchall()
 				cursor.close()
 				output=[] #use for return 
+				id_check_list=[]
 				 
-		for info in records:			
-			attraction={
-				"id": int(info[18]),
-				"name": info[2],
-				"category": info[11],
-				"description": info[17],
-				"address": info[20],
-				"transport": info[1],
-				"mrt": info[8],
-				"lat": float(info[16]),
-				"lng": float(info[4]),
-				"images": info[14].split(" ")
-			}
-			output.append(attraction)
+		for info in records:
+			if id_check_list==[]:  #first time
+				id_check_list.append(int(info[0]))
+				attraction={
+					"id": int(info[0]),
+					"name": info[1],
+					"category": info[2],
+					"description": info[3],
+					"address": info[4],
+					"transport": info[5],
+					"mrt": info[6],
+					"lat": float(info[7]),
+					"lng": float(info[8]),
+					"images": [info[9]]
+				}
+			elif int(info[0]) not in id_check_list:   #if id changed, append previous id's data
+				output.append(attraction)
+				id_check_list.append(int(info[0]))
+				attraction={
+					"id": int(info[0]),
+					"name": info[1],
+					"category": info[2],
+					"description": info[3],
+					"address": info[4],
+					"transport": info[5],
+					"mrt": info[6],
+					"lat": float(info[7]),
+					"lng": float(info[8]),
+					"images": [info[9]]
+				}
+			else:    #if id is same, just append img
+				attraction["images"].append(info[9])
+		output.append(attraction)  #for last data append		
+			
 		
 		if total_page==page:
 			nextPage=None
@@ -114,29 +154,41 @@ def attractions():
 @app.route("/api/attraction/<attractionId>")
 def query_attraction(attractionId):		
 	try:        
-		mySql_query = ("""SELECT * FROM site WHERE _id=%s""")
+		mySql_query = ("""
+            SELECT a._id, a.name, c.CAT, a.description, a.address, a.direction, m.MRT, a.latitude, a.longitude, i.file
+            FROM attraction as a 
+            INNER JOIN mrt as m on m.id=a.MRT_id
+            INNER JOIN category as c on c.id = a.CAT_id
+            INNER JOIN image as i on i.attr_id = a.id
+            WHERE a._id=%s
+        """)
 		connection = connection_pool.get_connection()
 		
 		if connection.is_connected():
 			cursor = connection.cursor()
 			cursor.execute(mySql_query, (attractionId,))       
-			records=cursor.fetchone()		
+			records=cursor.fetchall()		
+
 			if records:
+				img_list=[]
+				for record in records:
+					img_list.append(record[9])
 				attraction={
-					"id": int(records[18]),
-					"name": records[2],
-					"category": records[11],
-					"description": records[17],
-					"address": records[20],
-					"transport": records[1],
-					"mrt": records[8],
-					"lat": float(records[16]),
-					"lng": float(records[4]),
-					"images": records[14].split(" ")
+					"id": int(records[0][0]),
+					"name": records[0][1],
+					"category": records[0][2],
+					"description": records[0][3],
+					"address": records[0][4],
+					"transport": records[0][5],
+					"mrt": records[0][6],
+					"lat": float(records[0][7]),
+					"lng": float(records[0][8]),
+					"images": img_list
 				}
 				return {"data":attraction}, 200
 			else:
 				return {"error": True,"message": "景點編號不正確"}, 400
+
 		
 	except mysql.connector.Error as e:
 		print("attractions Error Message:", e)
@@ -150,7 +202,7 @@ def query_attraction(attractionId):
 @app.route("/api/categories")
 def categories():		
 	try:        
-		mySql_query = ("""SELECT CAT FROM site GROUP BY CAT""")
+		mySql_query = ("""SELECT CAT from category""")
 		connection = connection_pool.get_connection()
 		
 		if connection.is_connected():
