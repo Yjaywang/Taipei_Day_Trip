7cad46a37519efa7c6ce22f2c1c56f4dd99e575a
Jay Wang <E34013160@gmail.com>
2022-12-21 00:53:00 +0800

    fix thankyou page logout issue

--- application/static/js/controller/thankyou_controller.js
+++ application/static/js/controller/thankyou_controller.js
@@ -6,18 +6,25 @@ import controller from "../controller/member_controller.js";
 
 
 controller.init=async function() {
-  //page init
-  await model.init();
-  view.render(model.orderData, model.orderNumber);
   //member init
   memberView.signMenu();  
   await memberModel.checkSingIn(); 
   memberView.checkSingIn(memberModel.authData); 
   memberView.bookingPage(); 
-  await memberModel.checkBookingCount(); 
-  memberView.bookingCount(memberModel.bookingCounts); 
+
   memberView.addSignMenu();
   memberView.addEye();
+  if(!memberModel.authData.data){
+    //not login
+    memberView.needLogin();
+  }
+  //page init
+  await model.init();
+  view.render(model.orderData, model.orderNumber);
+
+  await memberModel.checkBookingCount(); 
+  memberView.bookingCount(memberModel.bookingCounts); 
+  
 
 }
 
